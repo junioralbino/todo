0b79f41ce1bd2648d0ad8bf37d048b464ea1d836
Suporte <junioralbino741@gmail.com>
2022-08-18 08:32:51 -0300

    Criando o input de enviar tarefa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import React, { useState }  from 'react';
+import { MdDelete } from 'react-icons/md';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = () => {
+  const ESCAPE_KEY = 13;
+  const ENTER_KEY = 27;
+
+  const initialTodos = [
+    {id: 1, title: 'Estudar React', checked: false},
+    {id: 2, title: 'Estudar Inglês', checked: true},
+    {id: 3, title: 'Estudar Devops', checked: false},
+  ];
+
+  const [todos, ] = useState(initialTodos);
+  const [value, setValue] = useState("");
+
+  const erase = (event) => {
+    setValue(event.target.value);
+  }
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const submit = () => {
+       console.log('submit', value);
+       erase();
+  }
+
+  const onKeyDown = (event) => {
+      if(event.which === ESCAPE_KEY){
+           submit();
+      }else if(event.which === ENTER_KEY){
+            erase();
+      }
+  }
+
+ return(
+  <section id="app" className='container'>
+      <header>
+          <h1 className='title'>Todo</h1>
       </header>
-    </div>
+
+      <section className='main'>
+          <input  className='new-todo' placeholder='O que precisa ser feito ?'
+          value={value} onChange={onChange} onKeyDown={onKeyDown} />
+
+          <ul className='todo-list'>
+            {
+              todos.map((todo) => (
+                  <li key={todo.id.toString()} >
+                    <span className="todo">{todo.title}</span>
+                    <button className="remove" type="button" ><MdDelete size={28}/></button>
+                   </li>
+              ))
+            }
+          </ul>
+      </section>
+
+  </section>
   );
 }
 
